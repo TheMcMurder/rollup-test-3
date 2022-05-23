adce12883d4cc6ae893df6dd32bb1134346b3cce
the_mcmurder <3059715+TheMcMurder@users.noreply.github.com>
2022-05-23 13:17:57 -0600

    fixing names

--- src/root.jsx
+++ src/root.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState } from 'react'
-import { Bar } from '../fake_node_module/bar-commonjs'
-import { Baz } from '../fake_node_module/baz-esm'
+import { Bar } from '../fake_node_module/bar-esm'
+import { Baz } from '../fake_node_module/baz-commonjs'
 import { Foo } from '../fake_node_module/foo-mixture'
 import { Button, SecondaryButton } from '../fake_node_module/multi'
 import ErrorBoundary from './ErrorBoundary'
